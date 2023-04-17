b5c0b976e216d827aa16e27879273dd33dd30594
Sascha <sascha@strandco.de>
2023-04-17 12:38:14 +0200

    Animate nav links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss', './logo.component.scss']
 })
 export class HeaderComponent {
   isMenuOpen: boolean = false;
+
+  navLinkData = [
+    { link: "./index.html", text: "Home" },
+    { link: "#aboutMe", text: "About me" },
+    { link: "#mySkills", text: "My skills" },
+    { link: "#myPortfolio", text: "My portfolio" },
+    { link: "#contactMe", text: "Contact me" },
+    // { link: "#", text: "Legal notice" },
+    // { link: "#", text: "Privacy policy" }
+  ]
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
